f57e246da6810f87feaa28651f47a28373aa86fb
Laurent BENAZET <benazetlaurent@gmail.com>
2022-03-10 02:06:41 +0100

    Added sent hour to messages

--- client/src/components/Messenger/Message.js
+++ client/src/components/Messenger/Message.js
@@ -1,13 +1,13 @@
 import React from "react";
 import {Chip} from "@material-ui/core";
-
+import moment from "moment";
 
 const Message = (props) => {
     const {message, mine} = props;
 
     return (
         <div style={{textAlign: mine?"right":"left", marginRight: mine ? "10px" : "0", marginLeft: mine ? "0" : "10px"}}>
-            <p style={{marginBottom:"0.3rem"}}>{mine ? "(Me) " : "" }{message.author.name}</p>
+            <p style={{marginBottom:"0.3rem"}}>{moment(message.createdAt, "x").format('HH:mm')} {mine ? "(Me) " : "" }{message.author.name}</p>
             <Chip style={{fontSize:"0.9rem"}} color={mine?"primary": "secondary"} label={message.content}/>
         </div>
     );
