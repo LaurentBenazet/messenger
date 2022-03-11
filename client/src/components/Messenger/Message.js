292de1205b22b59c4841b6dcbf2b8fa22bd3bb2d
Laurent BENAZET <benazetlaurent@gmail.com>
2022-03-11 19:47:49 +0100

    Added margins for messages dates

--- client/src/components/Messenger/Message.js
+++ client/src/components/Messenger/Message.js
@@ -1,15 +1,17 @@
 import React from "react";
 import moment from "moment";
 import Chip from "./Reusable/Chip";
+import '../../styles/Messenger/Message.css';
 
 const Message = (props) => {
+    //primaryColor is the color used for the current user's messages, the secondaryColor for the other participants
     const {message, mine, primaryColor='#3f51b5', secondaryColor='#f50057'} = props;
 
     return (
         <div className="message" style={{
             textAlign: mine ? "right" : "left",
-            marginRight: mine ? "10px" : "0",
-            marginLeft: mine ? "0" : "10px"
+            marginRight: mine ? "20px" : "0",
+            marginLeft: mine ? "0" : "20px"
         }}>
             <p className="message-date"
                style={{marginBottom: "0.3rem"}}>{moment(message.createdAt, "x").format('HH:mm')} {message.author.name}</p>
